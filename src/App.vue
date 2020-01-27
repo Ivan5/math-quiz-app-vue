ba07522880e05c3f715bb44ef2296b8bc2fe0082
Ivan5 <converse_ivan@hotmail.com>
2020-01-27 12:04:25 -0600

    Add a styles for the main page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Math Quiz App</h1>
+    {{ this.operator}}
+    <button @click="selectOperator('+')">
+      Addition
+      <br />+
+    </button>
+    <button @click="selectOperator('-')">
+      Subtraction
+      <br />-
+    </button>
+    <button @click="selectOperator('/')">
+      Division
+      <br />/
+    </button>
+    <button @click="selectOperator('*')">
+      Multiplication
+      <br />*
+    </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  data() {
+    return {
+      operator: null
+    };
+  },
+  methods: {
+    selectOperator(operator) {
+      this.operator = operator;
+    }
+  },
+  components: {}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  font-size: 24px;
+}
+
+button {
+  padding: 10px;
+  width: 160px;
+  height: 80px;
+  margin-right: 10px;
+  background-color: #10aeb2;
+  color: #fff;
+  font-size: 20px;
+  cursor: pointer;
+  vertical-align: top;
 }
 </style>
